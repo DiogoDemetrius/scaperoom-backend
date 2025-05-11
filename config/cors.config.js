d0f8066ce8d822b9bf92eab0e4aa8739c2aa7cdb
Diogo Demetrius <diogod.mmoreira@gmail.com>
2025-05-11 19:37:49 -0300

    att cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://scaperoom-fis-homologacao.vercel.app/', // ou o domínio do seu front-end em produção
+  origin: 'https://scaperoom-fis-homologacao.vercel.app', // ou o domínio do seu front-end em produção
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
