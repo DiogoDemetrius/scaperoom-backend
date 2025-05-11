93dd83619b22bd7ef8dbaa94ae72c8c18551da45
Diogo Demetrius <diogod.mmoreira@gmail.com>
2025-05-11 14:28:39 -0300

    att cors

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/game', gameRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+// Exporta o app para o Vercel
+module.exports = app;
