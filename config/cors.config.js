926446fe9e019e5cd3a95918d69f1b18e295c162
Diogo Demetrius <diogod.mmoreira@gmail.com>
2025-05-14 12:31:55 -0300

    att para connections

--- config/cors.config.js
+++ config/cors.config.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const allowedOrigins = [
   'http://localhost:3000',
   'https://scaperoom-fis-homologacao.vercel.app',
-  'https://scaperoom-fis.vercel.app'
+  'https://scaperoom-fis.vercel.app',
+  'https://m-fis-homologacao.vercel.app'
 ];
 
 const corsOptions = {
